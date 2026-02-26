96534b2031fd7f49138c555b200bda2bddab86b5
mariab <mariab>
2026-02-26 10:01:47 -0300

    26/02

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ export const tarefas = [
     {id: 1, titulo: "estudar node", concluida: false},
     {id: 2, titulo: "fazer banco de dados", concluida: false}
 ];
+
+tarefas.push({
+    id: 2,
+    titulo: "sofrer pelo corinthians",
+    concluida: true
+});
+
+console.table(tarefas);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import {tarefas} from "./index.js"
 
 const app = express();
 const PORT = 3000;
@@ -6,12 +7,16 @@ const PORT = 3000;
 app.use(express.json());
 
 app.get("/tarefas", (req, res)=> {
-    res.send("Olá mundo");
     res.status(200).json(tarefas);
 });
 
 app.post("/tarefas", (req, res) => {
     const { titulo } = req.body;
+
+    if (!titulo || titulo.trim() === "") {
+    return resizeBy.status(400).json({erro: "Titulo é obrigatorio."})
+};
+
 })
 
 app.listen(PORT, () => {
